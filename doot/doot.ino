57d70fb8f3ea160c14f5ab918b12b52860119091
Ritika Manzoor <ritika.manzoor1@gmail.com>
2016-08-09 15:40:35 -0500

    fixed some bugs that were impeding functionality

--- doot/doot.ino
+++ doot/doot.ino
@@ -2,10 +2,10 @@
 #include <Servo.h>              //Servo library
 
 
-int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
+/*int bluetoothTx = 2;  // TX-O pin of bluetooth mate, Arduino D2
 int bluetoothRx = 3;  // RX-I pin of bluetooth mate, Arduino D3
 
-SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);*/
 
 Servo servoTest;
 
@@ -20,13 +20,13 @@ void pause() {      //Stops & resets wheels
   servoRWh.writeMicroseconds(1500);
   servoLWh.writeMicroseconds(1500);
   delay(1000);
-}
+} //
+
 
 void stopPen(int secs) {   //Pen stays in position for x amount of time
   servoWrist.writeMicroseconds(1500);
   delay(secs);
 }
-
 
 void penup() {      //Pen up
   servoWrist.writeMicroseconds(1300);
@@ -37,9 +37,10 @@ void pendown() {    //Pen down
   servoWrist.writeMicroseconds(1700);
   delay(600);
 }
+//
 
 void turnRight() {  //Turn right
-  servoRWh.writeMicroseconds(2000); //right
+  servoRWh.writeMicroseconds(1500); //right
   servoLWh.writeMicroseconds(1700);
   delay(800);
   pause();
@@ -83,8 +84,10 @@ void drawSquare() {     //draw a square
   }
 }
 void circle() {     //draw a circle
-  for (int i = 0; i < 4; i++) {
-    turnLeft();
+  for (int i = 0; i < 6; i++) {
+  stopPen(7500);
+  servoRWh.writeMicroseconds(1700);
+  servoLWh.writeMicroseconds(1500);
   }
 
 }
@@ -115,7 +118,7 @@ void triangle() {     //draw a triangle
     pause();
     forward(500);
     pause();
-  }
+  } //
 }
 
 
@@ -136,13 +139,13 @@ long randNumber;    //Set up random
 
 void setup() {
 
-  Serial.begin(9600);  // Begin the serial monitor at 9600bps
+ // Serial.begin(9600);  // Begin the serial monitor at 9600bps
 
-  bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
+ /* bluetooth.begin(115200);  // The Bluetooth Mate defaults to 115200bps
   delay(100);               // Short delay, wait for the Mate to send back CMD
   bluetooth.println("U,9600,N");  // Temporarily Change the baudrate to 9600, no parity
   // 115200 can be too fast at times for NewSoftSerial to relay the data reliably
-  bluetooth.begin(9600);    // Start bluetooth serial at 9600
+  bluetooth.begin(9600);    // Start bluetooth serial at 9600*/
 
   servoTest.attach(13);
   servoWrist.attach(11);
@@ -153,19 +156,22 @@ void setup() {
 
 
 void loop() {
+  circle();
+  penup();
+  servoRWh.writeMicroseconds(1500); //right
+  servoLWh.writeMicroseconds(1700);
+  forward(250);
   /*servoRWh.writeMicroseconds(1500);
-  servoLWh.writeMicroseconds(1500);*/
+    servoLWh.writeMicroseconds(1500);*/
 
-  if (bluetooth.available())      // If the bluetooth sent any characters
+  /*if (bluetooth.available())      // If the bluetooth sent any characters
     {
      //pendown();
      //stopPen(5600);
      circle();
     // penup();
      //stopPen(500);
-     delay(500);
-
-     //servoTest.writeMicroseconds(1700);
+     //delay(500);
 
      // Send any characters the bluetooth prints to the serial monitor
      Serial.print((char)bluetooth.read());
@@ -176,7 +182,7 @@ void loop() {
      // Send any characters the Serial monitor prints to the bluetooth
      bluetooth.print((char)Serial.read());
     }
-    // and loop forever and ever!
+    // and loop forever and ever!*/
 
   /*pendown();
     stopPen(5600);
@@ -220,7 +226,6 @@ void loop() {
 
   /*pendown();      //draws a circle. set marker to left position
     stopPen(10680); */
-  //circle();
   //digitalWrite(LEDPIN, HIGH);
   //delay(500);
   /* penup();
